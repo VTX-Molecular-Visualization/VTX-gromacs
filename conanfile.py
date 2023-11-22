1c84efc479de477228cf377f5aea17e38b42150b
val <valent.guillaume@gmail.com>
2023-11-22 16:28:30 +0100

    Updating conanfile.py to enable build

--- conanfile.py
+++ conanfile.py
@@ -2,6 +2,7 @@ import os
 from conan import ConanFile
 from conan.tools.cmake import CMake, cmake_layout
 from conan.tools.scm import Git
+from conan.tools.files import copy
 
 class VtxGromacsRecipe(ConanFile):
     name = "vtx-gromacs"
@@ -20,10 +21,14 @@ class VtxGromacsRecipe(ConanFile):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
+    def generate(self):
+        # Conan seems to automatically add [recipe_folder]/include to the cmake generated include list for this package.
+        # However, gromacs keeps its header files alongside its sources, so we need to create an include folder ourselves
+        #   On a personal note, I don't like this design and maybe we could change it someway in the future
+        copy(self, "*.h", os.path.join(self.recipe_folder, "src"), os.path.join(self.recipe_folder, "include"))
+
     def layout(self):
         cmake_layout(self)    
-        # Add generated include dir for editable mode.
-        self.cpp.source.includedirs = ["include", os.path.join(self.folders.build, "include")]
         
     def build(self):
         cmake = CMake(self)
@@ -35,7 +40,6 @@ class VtxGromacsRecipe(ConanFile):
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = ["vtx-gromacs"]
-        if self.settings.os == "Windows":
-            self.cpp_info.system_libs.append('ws2_32')
+        self.cpp_info.names["generator_name"] = "Gromacs"
+        #self.cpp_info.libs = ["gromacs"] # If link fail, we should investigate this line
 
