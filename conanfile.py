df5994f9ac9b1388f4518e0d6e04c4b80621bde9
val <valent.guillaume@gmail.com>
2023-11-22 08:45:58 +0100

    WIP - updating version to match CMake's

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ from conan.tools.scm import Git
 
 class VtxGromacsRecipe(ConanFile):
     name = "vtx-gromacs"
-    version = "2023.4"
+    version = "2024.0"
     package_type = "library"
     
     settings = "os", "compiler", "build_type", "arch"
