801a75a5c1eef17659e5cb52c504ccc4d83c4fe6
Valentin <valent.guillaume@gmail.com>
2024-02-27 10:56:21 +0100

    Trying to fix gmx CI

--- conanfile.py
+++ conanfile.py
@@ -22,7 +22,7 @@ class VtxGromacsRecipe(ConanFile):
     
     generators = "CMakeDeps", "CMakeToolchain"
     
-    exports_sources = "CMakeLists.txt", "src/*", "cmake/*", "share/*", "tests/*", "api/*"
+    exports_sources = "CMakeLists.txt", "src/*", "cmake/*", "share/*", "tests/*", "api/*", "COPYING", "AUTHORS", "CITATION.cff"
         
     def config_options(self):
         if self.settings.os == "Windows":
